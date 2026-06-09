67a4c4b28e4b9d50c54f5b826c8758440298c85e
gulmirzayevasadbek552-vvv <gulmirzayevasadbek552@gmail.com>
2026-06-09 15:53:57 +0500

    Add: 15) 3 set: faqat bittasida uchragan elementlar [gulmirzayevasadbek552-vvv]

---
+++ lms_kod_topshiriqlari/Set_operatsiyalari__union__intersection__differenc/15__3_set__faqat_bittasida_uchragan_elementlar.py
@@ -0,0 +1,11 @@
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+C = set(map(int, input().split()))
+only_A = A - B - C
+only_B = B - A - C
+only_C = C - A - B
+result = only_A | only_B | only_C
+if result:
+    print(' '.join(map(str, sorted(result))))
+else:
+    print("BO'SH")
