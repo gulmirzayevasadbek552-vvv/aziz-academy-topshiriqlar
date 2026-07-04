b9d527151f6af6794ca58119df5b2405fa902e90
gulmirzayevasadbek552-vvv <gulmirzayevasadbek552@gmail.com>
2026-07-04 17:11:21 +0500

    Add AI task: Teskari yozish

---
+++ ai_topshiriqlar/Teskari_yozish.py
@@ -0,0 +1,9 @@
+# Teskari yozish
+# Kurs: Dasturlash / IT
+# Mavzu: String metodlari — 1: lower, upper, strip, title
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+w = input().split()
+w.sort()
+print(' '.join(w))
