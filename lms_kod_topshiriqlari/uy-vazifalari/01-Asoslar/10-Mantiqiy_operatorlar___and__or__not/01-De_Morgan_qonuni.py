c7fd18450805a82f650de22a21c15d2ad1fc703f
gulmirzayevasadbek552-vvv <gulmirzayevasadbek552@gmail.com>
2026-07-07 16:49:52 +0500

    Add: De Morgan qonuni [gulmirzayevasadbek552-vvv]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-De_Morgan_qonuni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-De_Morgan_qonuni.py
@@ -1,4 +1,4 @@
-print("True")
-print("True")
-print("False")
-print("False")
+print(not (True and False))
+print((not True) or (not False))
+print(not (True or False))
+print((not True) and (not False))
