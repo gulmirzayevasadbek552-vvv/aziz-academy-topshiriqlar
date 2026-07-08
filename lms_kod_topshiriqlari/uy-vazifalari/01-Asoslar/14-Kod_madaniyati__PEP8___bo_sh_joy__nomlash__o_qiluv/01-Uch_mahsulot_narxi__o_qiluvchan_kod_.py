0f43fd1fd13c7ba23ede61f44170ed268fb17d01
gulmirzayevasadbek552-vvv <gulmirzayevasadbek552@gmail.com>
2026-07-08 08:03:53 +0500

    Add: Uch mahsulot narxi (o'qiluvchan kod) [gulmirzayevasadbek552-vvv]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Uch_mahsulot_narxi__o_qiluvchan_kod_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Uch_mahsulot_narxi__o_qiluvchan_kod_.py
@@ -1,4 +1,6 @@
-a = int(input())
-b = int(input())
-c = int(input())
-print(a + b + c)
+narx_1 = int(input())
+narx_2 = int(input())
+narx_3 = int(input())
+
+jami = narx_1 + narx_2 + narx_3 
+print(jami)
