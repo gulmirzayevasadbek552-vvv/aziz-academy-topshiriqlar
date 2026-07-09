87deb2e4097fc68736f0bf2449a534cd224cb373
gulmirzayevasadbek552-vvv <gulmirzayevasadbek552@gmail.com>
2026-07-09 16:20:46 +0500

    Add: Elektr energiya to'lovini hisoblash [gulmirzayevasadbek552-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
@@ -0,0 +1,9 @@
+k = int(input())
+if k < 0:
+    print("Notogri qiymat")
+elif k <= 100:
+    print(k * 450)
+elif k <= 200:
+    print(100 * 450 + (k - 100) * 600)
+else:
+    print(100 * 450 + 100 * 600 + (k - 200) * 900)
